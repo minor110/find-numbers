e97ece450622fbb4e3d31cecd9c6dfe3c5068c6c
Sergey Filippov <filippov.sergey110@gmail.com>
2022-02-20 12:52:36 +0300

    Initial Commit

---
+++ FindNumber/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FindNumber
+//
+//  Created by Sergey Filippov on 20.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
